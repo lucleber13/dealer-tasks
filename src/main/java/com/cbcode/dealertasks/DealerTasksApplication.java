8fd05d530aa3f290aedf441936e261edf4876e18
cleber <lucleber13@hotmail.com>
2025-02-16 15:35:11 +0000

    first commit

---
+++ src/main/java/com/cbcode/dealertasks/DealerTasksApplication.java
@@ -0,0 +1,13 @@
+package com.cbcode.dealertasks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DealerTasksApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DealerTasksApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cbcode/dealertasks/DealerTasksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cbcode.dealertasks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DealerTasksApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
